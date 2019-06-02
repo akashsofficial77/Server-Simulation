d7aeee2092fdf42f4ae750fa07ba0d4912513e5f
Srivastava_Akash_001388877 <srivastava.aka@husky.neu.edu>
2019-06-02 16:01:13 -0400

    Dispatcher update

--- ElasticComputing/src/elasticcomputing/Dispatcher.java
+++ ElasticComputing/src/elasticcomputing/Dispatcher.java
@@ -4,12 +4,30 @@
  * and open the template in the editor.
  */
 package elasticcomputing;
-
+import java.util.*;
 /**
  *
  * @author tinyteddybear
  */
 public class Dispatcher {
+    
+    class RunnableImp implements Runnable{
+        Queue q;
+        
+        public RunnableImp(Queue q)
+        {
+            this.q=q;
+        }
+        
+        public void run()
+        {
+            while(!q.isEmpty())
+            {
+                System.out.println(Thread.currentThread().getName()+":"+q.poll());
+            }
+                
+        }
+    }
     public static void main (String args[])
     {
         
